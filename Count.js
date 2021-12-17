c5082c80a016b6b9d5ca4412431c27b1e87558cb
unknown <thiagottschneider@gmail.com>
2021-12-17 15:45:56 -0300

    Arithmetics

---
+++ Count.js
@@ -0,0 +1,48 @@
+let x = 10, y = 2;
+
+//Soma
+let Resolução = x + y;
+console.log(` A soma de: ${x} mais ${y} é igual á: ${Resolução}`);
+
+//Subtração
+Resolução = x - y;
+console.log(` A Subtração de: ${x} mais ${y} é igual á: ${Resolução}`);
+
+//Multiplicação
+Resolução = x * y;
+console.log(` A Multiplicação de: ${x} mais ${y} é igual á: ${Resolução}`);
+
+
+//Divisão
+Resolução = x / y;
+console.log(` A Divisão de: ${x} mais ${y} é igual á: ${Resolução}`);
+
+
+
+const line_1 = "";
+const line_2 = "Áreas das Formas Geometricas";
+console.log(line_1);
+console.log(line_2);
+console.log(line_1);
+
+
+let lado = 5, largura = 16, comprimento = 6, base = 12, altura = 8, resposta = 0;
+
+//Área do Quadrado//
+
+resposta = lado * lado;
+console.log(`A área do quadrado é de: ${resposta}`);
+
+//Área do Retangulo//
+
+resposta = largura * comprimento;
+console.log(`A área do retangulo é de: ${resposta}`);
+
+//Área do Triangulo//
+
+resposta = base * altura /2
+console.log(`A área do triangulo é de: ${resposta}`);
+
+
+
+
