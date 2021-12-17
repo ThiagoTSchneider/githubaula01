3a573b0c76420a0ffbb02d55cee2bfa5faf3f282
unknown <thiagottschneider@gmail.com>
2021-12-17 00:11:55 -0300

    Javascript Learning

---
+++ Var2.js
@@ -0,0 +1,23 @@
+const nome = "Thiago Schneider";
+
+//Detalhes Thiago Schneider//
+let idade = 15;
+let altura = 1.87;
+const cidade_natal = "Blumenau";
+let cidade_populaçao = "361.800";
+
+console.log(nome);
+console.log(idade);
+console.log(altura,`Em metros`);
+console.log(`A cidade natal de Thiago Schneider é ${cidade_natal}.`);
+
+//Cidade informaçôes//
+
+console.log(`Em ${cidade_natal} Há um total de ${cidade_populaçao} Habitantes.`);
+
+cidade_populaçao = "361.855"
+
+console.log(`Desculpa um total de ${cidade_populaçao} Habitantes`);
+
+
+
